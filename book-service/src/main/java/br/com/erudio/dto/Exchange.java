3fc61dc7149980d8313092c5a431f0e3d29cd189
Gustavo12386 <gucalderaro@hotmail.com>
2025-08-05 13:28:39 -0300

    adicionando arquivos de book-service

---
+++ book-service/src/main/java/br/com/erudio/dto/Exchange.java
@@ -0,0 +1,104 @@
+package br.com.erudio.dto;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Exchange implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;	
+	private String from;	
+	private String to;
+	private BigDecimal conversionFactor;	
+	private Double convertedValue;	
+	private String environment;
+	
+	public Exchange() {}
+	
+	public Exchange(Long id, String from, String to, BigDecimal conversionFactor, Double convertedValue,
+			String environment) {	
+		this.id = id;
+		this.from = from;
+		this.to = to;
+		this.conversionFactor = conversionFactor;
+		this.convertedValue = convertedValue;
+		this.environment = environment;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public BigDecimal getConversionFactor() {
+		return conversionFactor;
+	}
+
+	public void setConversionFactor(BigDecimal conversionFactor) {
+		this.conversionFactor = conversionFactor;
+	}
+
+	public Double getConvertedValue() {
+		return convertedValue;
+	}
+
+	public void setConvertedValue(Double convertedValue) {
+		this.convertedValue = convertedValue;
+	}
+
+	public String getEnvironment() {
+		return environment;
+	}
+
+	public void setEnvironment(String environment) {
+		this.environment = environment;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(conversionFactor, convertedValue, environment, from, id, to);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Exchange other = (Exchange) obj;
+		return Objects.equals(conversionFactor, other.conversionFactor)
+				&& Objects.equals(convertedValue, other.convertedValue)
+				&& Objects.equals(environment, other.environment) && Objects.equals(from, other.from)
+				&& Objects.equals(id, other.id) && Objects.equals(to, other.to);
+	}
+	
+	
+   
+}
